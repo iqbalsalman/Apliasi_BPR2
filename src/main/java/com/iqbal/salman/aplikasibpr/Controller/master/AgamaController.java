9404a78d259c16378486100ed9876c8a13a19dbd
iqbalsalman <iqbalsalman1707@gmail.com>
2018-02-19 12:49:39 +0700

    Update Crud Alert dial

--- src/main/java/com/iqbal/salman/aplikasibpr/Controller/master/AgamaController.java
+++ src/main/java/com/iqbal/salman/aplikasibpr/Controller/master/AgamaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -21,34 +22,48 @@ public class AgamaController {
 
 
     /**
-     *
      * localhost:8080/agama/   localhost:8080/agama/list
+     *
      * @return
      */
 
     @GetMapping(value = {"/", "/list"})
-    public  String lisAgama(ModelMap params){
-        params.addAttribute("lisAgama",agamaService.findAll());
-        return  "/pages/agama/list";
+    public String lisAgama(ModelMap params) {
+        params.addAttribute("lisAgama", agamaService.findAll());
+        return "/pages/agama/list";
     }
+
     @GetMapping("/form")
     public String formAgama(Agama agama, ModelMap params) {
-                params.addAttribute("agama", agama);
-                return "/pages/agama/form";
-            }
+        params.addAttribute("agama", agama);
+        return "/pages/agama/form";
+    }
+    @GetMapping("/form/{id}")
+    public String formAgama(@PathVariable (value = "id") String idup, ModelMap params, RedirectAttributes redirectAttributes) {
+        Agama agama = agamaService.findById(idup);
+        if (agama != null) {
+            params.addAttribute("agama", agama);
+            return "/pages/agama/form";
+        }else{
+//            params.addAttribute("agama",new Agama());
+            redirectAttributes.addFlashAttribute("notAvailabel","Data Tidak ditemukan");
+            return "redirect:/agama/list";
+        }
+    }
 
-            @PostMapping("/submit")
-    public String submitAgama(@ModelAttribute Agama agama) {
-                agama.setCreatedDate(Timestamp.valueOf(LocalDateTime.now()));
-                agama.setCreatedBy("admin");
-                agamaService.save(agama);
-                return "redirect:/agama/list";
-            }
+    @PostMapping("/submit")
+    public String submitAgama(@ModelAttribute Agama agama,RedirectAttributes redirectAttributes) {
+        agama.setCreatedDate(Timestamp.valueOf(LocalDateTime.now()));
+        agama.setCreatedBy("admin");
+        agamaService.save(agama);
+        redirectAttributes.addFlashAttribute("alertSuccess", "Data berhasil disimpan");
+        return "redirect:/agama/list";
+    }
 
 
-    @RequestMapping("product/delete/{id}")
+    @RequestMapping("/hapus/delete/{id}")
     public String delete(@PathVariable(value = "id") String id) {
-           agamaService.delete(id);
+        agamaService.delete(id);
         return "redirect:/agama/list";
     }
 
